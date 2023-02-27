46fc8aa38af701d09e54e628562252f28f9fa8ce
Lily Fitz <104954859+lily-fitz@users.noreply.github.com>
2023-02-27 13:00:18 -0500

    update

--- script.js
+++ script.js
@@ -8,6 +8,6 @@
 
 //// Variable: The navigation list is/isn't visible
 
-//// When we click the button:
-////   If the menu opens, we set the variable to true (is visible).
-////   If the menu closes, we set the variable to false (isn't visible).
+//// Conditional: When we click the button:
+////     If the menu opens, we set the variable to true (is visible).
+////     If the menu closes, we set the variable to false (isn't visible).
